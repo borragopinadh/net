ad6cc93ca3028b17a4b6831f73c5f8c653bacf80
borragopinadh <123790349+borragopinadh@users.noreply.github.com>
2023-11-26 13:22:41 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a,b;
+            a=5; b=10;
+            Console.WriteLine("addition" + (a + b));
+            Console.WriteLine("subtraction"+(a-b));
+            Console.WriteLine("multiplication"+(a*b));
+            Console.WriteLine("division"+(a/b));
+            Console.ReadLine();
+            }
+    }
+}
