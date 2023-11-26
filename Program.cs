b47befdd1e6636a630d0fa626e66bb5e6e25d305
borragopinadh <123790349+borragopinadh@users.noreply.github.com>
2023-11-26 13:24:48 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,19 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace ConsoleApp2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int a,b;
-            a=5; b=10;
-            Console.WriteLine("addition" + (a + b));
-            Console.WriteLine("subtraction"+(a-b));
-            Console.WriteLine("multiplication"+(a*b));
-            Console.WriteLine("division"+(a/b));
-            Console.ReadLine();
-            }
+            int a, b, c;
+            a = 5;
+            b = 7;
+            Console.WriteLine("befpre swaping");
+            Console.WriteLine("a=" + a + "b=" + b);
+            Console.WriteLine("after swaping");
+            c = a;
+            a = b;
+            b = c;
+            Console.WriteLine("a=" + a + "b=" + b);
+        }
     }
 }
